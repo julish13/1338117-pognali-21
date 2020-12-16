480c42608175dc1264c3b8decc31a4c5dabe0f3c
Julia Kolupaeva <jkolupaeva@gmail.com>
2020-12-16 11:28:13 +0300

    start adapat users for tablet

--- source/js/menu.js
+++ source/js/menu.js
@@ -68,13 +68,13 @@
   });
 
 
-  // let lastTimeout = null;
-  // window.onresize = function(){
-  //   clearTimeout(lastTimeout);
-  //   localStorage.setItem('scrollPosition', window.scrollY);
-  //   lastTimeout = setTimeout(function () {
-  //     location.reload();
-  //     window.scrollTo(0, localStorage.getItem('scrollPosition'));
-  //   }, 100);
-  // };
+  let lastTimeout = null;
+  window.onresize = function(){
+    clearTimeout(lastTimeout);
+    localStorage.setItem('scrollPosition', window.scrollY);
+    lastTimeout = setTimeout(function () {
+      location.reload();
+      window.scrollTo(0, localStorage.getItem('scrollPosition'));
+    }, 100);
+  };
 })();
