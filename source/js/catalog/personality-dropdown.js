f5b858cf123d4bbc79d85cf450d008972eaca184
Julia Kolupaeva <jkolupaeva@gmail.com>
2021-01-08 12:39:12 +0300

    accessability

--- source/js/catalog/personality-dropdown.js
+++ source/js/catalog/personality-dropdown.js
@@ -1,6 +1,11 @@
 'use strict';
 
 (() => {
+  const EVENT_KEYS = {
+    ESC: `Escape`,
+    ENTER: `Enter`
+  };
+
   const fieldElementsCollection = document.querySelectorAll(`.filter-personality-field`);
 
   for (const fieldElement of fieldElementsCollection) {
@@ -9,17 +14,37 @@
     fieldElement.classList.remove(`filter-personality-field--nojs`);
     fieldElement.classList.add(`filter-personality-field--closed`);
 
+    const openDropdown = () => {
+      fieldElement.classList.remove(`filter-personality-field--closed`);
+      fieldElement.classList.add(`filter-personality-field--opened`);
+    };
+
+    const closeDropdown = () => {
+      fieldElement.classList.remove(`filter-personality-field--opened`);
+      fieldElement.classList.add(`filter-personality-field--closed`);
+    };
+
     const onClickDropdownFunction = () => {
       if (!fieldElement.classList.contains(`filter-personality-field--opened`)) {
-        fieldElement.classList.remove(`filter-personality-field--closed`);
-        fieldElement.classList.add(`filter-personality-field--opened`);
+        openDropdown();
       } else {
-        fieldElement.classList.remove(`filter-personality-field--opened`);
-        fieldElement.classList.add(`filter-personality-field--closed`);
+        closeDropdown();
       }
     }
 
     headerElement.addEventListener(`click`, onClickDropdownFunction);
+    headerElement.addEventListener(`keydown`, (evt) => {
+      if (evt.key === EVENT_KEYS.ENTER) {
+        evt.preventDefault();
+        openDropdown();
+      }
+    });
+    headerElement.addEventListener(`keydown`, (evt) => {
+      if (evt.key === EVENT_KEYS.ESC) {
+        evt.preventDefault();
+        closeDropdown();
+      }
+    });
   }
 
 })();
